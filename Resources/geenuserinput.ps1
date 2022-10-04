0afc1a39ecb6280e2a39bb0e1304e3b95aa7753e
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-10-04 12:03:59 +0200

    Create geenuserinput.ps1

---
+++ Resources/geenuserinput.ps1
@@ -0,0 +1,9 @@
+$NewName = Read-Host -Prompt "Wat is de naam van de PC?(bijv. WS01 of LT01)"
+Rename-Computer -NewName "$NewName"
+Set-TimeZone -Name "W. Europe Standard Time"
+& 'H:\Resources\Ninite.exe'
+TAKEOWN /f C:\Windows.old\*.*
+ICACLS C:\Windows.old\*.* /Grant 'System:(F)'
+Remove-Item C:\Windows.old\*.*
+Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
+powershell.exe -ExecutionPolicy UnRestricted -File H:\Resources\nietgebruiken.ps1
