60e4377d272f7c826ecb64026ae45b5dc5425571
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-10-04 11:53:06 +0200

    Update nietgebruiken.ps1
    variabele ScriptDrive veranderd naar H: in script paden
    vraag naar schijf letter weggehaald

--- Resources/nietgebruiken.ps1
+++ Resources/nietgebruiken.ps1
@@ -1,18 +1,16 @@
-$ScriptDrive= Read-Host -Prompt "Wat is de schijf letter van deze usb?(bijv X:)"
-ECHO "De schijf letter is $ScriptDrive"
 Install-Module PSWindowsUpdate
 Add-WUServiceManager -MicrosoftUpdate
 Get-WindowsUpdate -AcceptAll -Install
 $UpdateFailed= Read-Host -Prompt "Was er een foutmelding tijdens het updaten?(ja/nee)"
 if ($UpdateFailed -eq 'ja') {
-    powershell -ExecutionPolicy Bypass -File $ScriptDrive\Resources\nietgebruiken.ps1}
+    powershell -ExecutionPolicy Bypass -File H:\Resources\nietgebruiken.ps1}
 elseif ($UpdateFailed -eq 'nee') {
      echo "Ok."
 }
 else {
       echo "Dat is een fout antwoord, het script start nu opnieuw."
       Start-Sleep -Seconds 3
-      powershell -ExecutionPolicy Bypass -File $ScriptDrive\Resources\nietgebruiken.ps1
+      powershell -ExecutionPolicy Bypass -File H:\Resources\nietgebruiken.ps1
 }
 Start-Service w32time
 w32tm /register
@@ -22,4 +20,4 @@ Write-Host "De tijd en datum zijn nu goed gezet."
 wget https://trivision.nl/downloads/TeamViewer_Host_Setup.exe -OutFile "C:\TeamViewer.exe"
 & "C:\TeamViewer.exe"
 Write-Host "TeamViewer is nu geinstalleerd
-powershell -ExecutionPolicy Bypass -File $ScriptDrive\Resources\Extragebruikersjaofnee.ps1
+powershell -ExecutionPolicy Bypass -File H:\Resources\Extragebruikersjaofnee.ps1
