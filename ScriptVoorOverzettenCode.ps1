2e2464a59abb484e542cc098f7bab4f8cd46e553
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-10-04 12:06:09 +0200

    Delete ScriptVoorOverzettenCode.ps1

--- ScriptVoorOverzettenCode.ps1
+++
@@ -1,8 +0,0 @@
-$NewName = Read-Host -Prompt "Wat is de naam van de PC?(bijv. WS01 of LT01)"
-Rename-Computer -NewName "$NewName"
-Set-TimeZone -Name "W. Europe Standard Time"
-& 'H:\Resources\Ninite.exe'
-TAKEOWN /f C:\Windows.old\*.*
-ICACLS C:\Windows.old\*.* /Grant 'System:(F)'
-Remove-Item C:\Windows.old\*.*
-Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
