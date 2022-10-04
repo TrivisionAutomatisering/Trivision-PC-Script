81069e45e5b1dc6df64a1e00dd0429248167ad18
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-10-04 14:10:43 +0200

    Update Semi-Auto install.ps1
    fout in script verbeterd

--- Semi-Auto install.ps1
+++ Semi-Auto install.ps1
@@ -4,7 +4,7 @@ Write-Host "U bent niet als TriAdmin ingelogd, log in als TriAdmin en probeer op
 timeout /t 5
 exit
 }
-Read-Host -Prompt $ScriptDrive="Wat is de schijf letter van deze usb?(bijv. D)"
+$ScriptDrive = Read-Host -Prompt "Wat is de schijf letter van deze usb?(bijv. D)"
 Write-Host $ScriptDrive
 Get-Partition -DriveLetter $ScriptDrive| Set-Partition -NewDriveLetter H
 powershell.exe -ExecutionPolicy UnRestricted -File H:\Resources\geenuserinput.ps1
