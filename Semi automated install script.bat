c0d1b6d2c698edafb369ea95a30061d5a24706c3
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-10-04 12:05:55 +0200

    Delete Semi automated install script.bat

--- Semi automated install script.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo "Na dit script word de pc opnieuw opgestart."
-IF NOT %username%==TriAdmin (echo "U bent niet als TriAdmin ingelogd, log in als TriAdmin en probeer opnieuw." & timeout /t 5 & exit)
-set /p ScriptDrive="Wat is de drive letter van deze usb?(bijv. X:)"
-echo %ScriptDrive%
-powershell.exe -ExecutionPolicy UnRestricted -File %ScriptDrive%\Resources\nietgebruiken.ps1
