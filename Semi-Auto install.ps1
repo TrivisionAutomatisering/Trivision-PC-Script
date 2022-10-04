dc058d682b690fbae13125e882dd1fcdc2f3a38c
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-10-04 11:46:59 +0200

    Update Semi-Auto install.ps1

--- Semi-Auto install.ps1
+++ Semi-Auto install.ps1
@@ -4,7 +4,7 @@ Write-Host "U bent niet als TriAdmin ingelogd, log in als TriAdmin en probeer op
 timeout /t 5
 exit
 }
-Read-Host -Prompt $ScriptDrive="Wat is de drive letter van deze usb?(bijv. D)"
+Read-Host -Prompt $ScriptDrive="Wat is de schijf letter van deze usb?(bijv. D)"
 Write-Host $ScriptDrive
 Get-Partition -DriveLetter $ScriptDrive| Set-Partition -NewDriveLetter H
 powershell.exe -ExecutionPolicy UnRestricted -File %ScriptDrive%\Resources\nietgebruiken.ps1
