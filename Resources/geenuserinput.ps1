fcc560a4d24ad2e5be433e40763668383af48717
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-10-10 12:24:47 +0200

    Add files via upload

--- Resources/geenuserinput.ps1
+++ Resources/geenuserinput.ps1
@@ -1,7 +1,9 @@
 $NewName = Read-Host -Prompt "Wat is de naam van de PC?(bijv. WS01 of LT01)"
 Rename-Computer -NewName "$NewName"
-Set-TimeZone -Name "W. Europe Standard Time"
-& 'H:\Resources\Ninite.exe'
+Enable-BitLocker -MountPoint "C:" -RecoveryPasswordProtector
+Start-Sleep 2
+(Get-BitLockerVolume -MountPoint C).KeyProtector > H:/BitlockerKey$NewName.txt
+tzutil /s "W. Europe Standard Time"
 TAKEOWN /f C:\Windows.old\*.*
 ICACLS C:\Windows.old\*.* /Grant 'System:(F)'
 Remove-Item C:\Windows.old\*.*
--- Resources/nietgebruiken.ps1
+++ Resources/nietgebruiken.ps1
@@ -1,5 +1,7 @@
 Install-Module PSWindowsUpdate
-Add-WUServiceManager -MicrosoftUpdate
+$ServiceManager = New-Object -ComObject "Microsoft.Update.ServiceManager"
+$ServiceManager.ClientApplicationID = "My App"
+$NewService = $ServiceManager.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"")
 Get-WindowsUpdate -AcceptAll -Install
 $UpdateFailed= Read-Host -Prompt "Was er een foutmelding tijdens het updaten?(ja/nee)"
 if ($UpdateFailed -eq 'ja') {
@@ -8,7 +10,7 @@ elseif ($UpdateFailed -eq 'nee') {
      echo "Ok."
 }
 else {
-      echo "Dat is een fout antwoord, het script start nu opnieuw."
+      echo "Dat is een fout antwoord, dit gedeelte van het script start nu opnieuw."
       Start-Sleep -Seconds 3
       powershell -ExecutionPolicy Bypass -File H:\Resources\nietgebruiken.ps1
 }
@@ -19,12 +21,9 @@ w32tm /resync /force
 Write-Host "De tijd en datum zijn nu goed gezet."
 wget https://trivision.nl/downloads/TeamViewer_Host_Setup.exe -OutFile "C:\TeamViewer.exe"
 & "C:\TeamViewer.exe"
-Write-Host "TeamViewer is nu aan het installeren"
-timeout /t 15
 wget https://ninite.com/7zip-adoptjdkx8-chrome-foxit/ninite.exe -OutFile "C:\ninite.exe"
 & "C:\ninite.exe"
-Write-Host "ninite is nu aan het installeren"
-timeout /t 15
+Read-Host 'Druk op Enter als TeamViewer en ninite geinstalleerd zijn.'
 Remove-Item "C:\ninite.exe"
-Remove-Item "TeamViewer.exe"
+Remove-Item "C:\TeamViewer.exe"
 powershell -ExecutionPolicy Bypass -File H:\Resources\Extragebruikersjaofnee.ps1
