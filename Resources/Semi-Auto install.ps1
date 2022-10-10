c34f24ef7d720a21a83e258a76c2290442db87ba
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-10-10 15:59:05 +0200

    Uitleg segmenten script

--- Resources/Semi-Auto install.ps1
+++ Resources/Semi-Auto install.ps1
@@ -1,5 +1,8 @@
+#Neemt variabele over vanuit het cmd start script
 param ([string]$ScriptDrive)
 Write-Host "Na dit script word de pc opnieuw opgestart."
+#Verandert usb schijf letter naar H:
 Get-Partition -DriveLetter $ScriptDrive | Set-Partition -NewDriveLetter H
 timeout /t 5
+#Start volgende deel van script
 powershell.exe -ExecutionPolicy UnRestricted -File H:\Resources\geenuserinput.ps1
