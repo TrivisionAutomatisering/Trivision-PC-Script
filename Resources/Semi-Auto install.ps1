03158e772dc56841c02cacf424a70ad72b274c92
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-10-18 16:06:00 +0200

    Internet test
    test of de pc de google primaire dns kan bereiken

--- Resources/Semi-Auto install.ps1
+++ Resources/Semi-Auto install.ps1
@@ -1,8 +1,18 @@
 #Neemt variabele over vanuit het cmd start script
 param ([string]$ScriptDrive)
+#Stopt het script als de google dns niet bereikt kan worden
+$InternetVerbinding = Write-Host (Test-Connection 8.8.8.8 -quiet)
+if($InternetVerbinding -eq "False"){
+   Write-Host "Er is geen internetverbinding"
+   Timeout /t 5
+   stop-process -Id $PID
+}
+#
 Write-Host "Na dit script word de pc opnieuw opgestart."
 #Verandert usb schijf letter naar H:
 Get-Partition -DriveLetter $ScriptDrive | Set-Partition -NewDriveLetter H
 timeout /t 5
+#De-blokkeert ps1 bestanden voor windows 10
+gci H:\Resources | Unblock-File
 #Start volgende deel van script
 powershell.exe -ExecutionPolicy UnRestricted -File H:\Resources\geenuserinput.ps1
