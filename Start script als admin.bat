7a04455be3e16d4d7d95d358b016e0540a4ed259
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-10-18 09:31:04 +0200

    compatibiliteit met windows 10
    hierdoor krijg je niet steeds een melding of je een powershell script wilt starten op windows 10

--- Start script als admin.bat
+++ Start script als admin.bat
@@ -1,3 +1,4 @@
 @echo off
 set /p "ScriptDrive=Wat is de schijf letter van de usb?(bijv. D)(zonder :)"
+powershell.exe gci -Path %ScriptDrive%:\ -Depth 1 | Unblock-File
 powershell.exe -ExecutionPolicy UnRestricted -File "%ScriptDrive%:\Resources\Semi-Auto install.ps1" "%ScriptDrive%"
