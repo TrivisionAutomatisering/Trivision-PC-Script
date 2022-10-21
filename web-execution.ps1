385c1397e7f7ec413975ae4e61b99591d96e5187
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-10-21 16:27:11 +0200

    experimentele web versie

---
+++ web-execution.ps1
@@ -0,0 +1,10 @@
+﻿#Neemt variabele over vanuit het cmd start script
+param ([string]$ScriptDrive)
+#Verandert usb schijf letter naar H:
+Get-Partition -DriveLetter $ScriptDrive | Set-Partition -NewDriveLetter H
+mkdir C:\Temp -ErrorAction Ignore
+wget https://github.com/MitchellTrivision/Trivision-PC-Script/releases/latest/download/PcScript.zip -OutFile C:\Temp\PcScript.zip
+Expand-Archive C:\Temp\PcScript.zip -DestinationPath H:\
+Remove-Item "C:\Temp" -Recurse
+Unblock-File H:/*.ps1
+powershell.exe -ExecutionPolicy UnRestricted -File "H:\Resources\Semi-Auto install.ps1"
