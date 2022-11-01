89b01a09e94a817d65e7c2c4ac0e10120dfe83b4
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-11-01 14:16:27 +0100

    specs gedeelte verbeterd

--- Resources/geenuserinput.ps1
+++ Resources/geenuserinput.ps1
@@ -3,29 +3,29 @@ if(((Get-BitLockerVolume | Where-Object -Property MountPoint -Contains C:).Prote
 Enable-BitLocker -MountPoint "C:" -RecoveryPasswordProtector
 }
 #Slaat specs en bitlocker op als variabele
-$Model = Get-ComputerInfo CsModel | Select -ExpandProperty CsModel
-$CPU = Get-ComputerInfo CsProcessors | Select -ExpandProperty CsProcessors | Select -ExpandProperty Name
-$SerieNummer = Get-ComputerInfo BiosSeralnumber | Select -ExpandProperty BiosSeralNumber
-$SchijfNaam = Get-Disk | Where-Object -Property Number -Contains 0 | Select -ExpandProperty FriendlyName
-$SchijfGrootte = Write-Host ([math]::Round((get-disk -number 0 | Select -ExpandProperty Size)/1073741824)) GB
-$TotaalGeheugen = [math]::Round((Get-CimInstance -Class CIM_PhysicalMemory -ErrorAction Stop | Select -ExpandProperty Capacity)/1073741824)
-$GeheugenMHZ = Get-CimInstance -Class CIM_PhysicalMemory -ErrorAction Stop | Select -ExpandProperty Speed
-$VolledigeRamInfo = Write-Host $TotaalGeheugen GB  "@" $GeheugenMHZ MHZ
-$BitLockerID = (Get-BitLockerVolume -MountPoint C).KeyProtector | Where-Object RecoveryPassword | Select -ExpandProperty KeyProtectorId
+$Model            = (Get-CimInstance Win32_ComputerSystem).Model
+$CPU              = (Get-CimInstance Win32_Processor -Property Name).Name
+$SerieNummer      = (Get-CimInstance Win32_BIOS).SerialNumber
+$SchijfNaam       = (Get-CimInstance Win32_DiskDrive | Where-Object -Property DeviceID -Contains \\.\PHYSICALDRIVE0).Caption
+$SchijfGrootte    = [math]::Round((Get-CimInstance Win32_DiskDrive -Filter 'DeviceID="\\\\.\\PHYSICALDRIVE0"').Size / 1GB)
+$TotaalGeheugen   = [math]::Round((Get-CimInstance -Class CIM_PhysicalMemory -ErrorAction Stop | Measure-Object Capacity -Sum).Sum / 1GB)
+$GeheugenMHZ      = (Get-CimInstance -Class CIM_PhysicalMemory -ErrorAction Stop | Measure-Object Speed -Minimum).Minimum
+$BitLockerID      = (Get-BitLockerVolume -MountPoint C).KeyProtector | Where-Object RecoveryPassword | Select -ExpandProperty KeyProtectorId
 $BitLockerSleutel = (Get-BitLockerVolume -MountPoint C).KeyProtector | Where-Object RecoveryPassword | Select -ExpandProperty RecoveryPassword
+
 #Verandert systeem naam
 $NewName = Read-Host -Prompt "Wat is de naam van de PC?(bijv. WS01 of LT01)"
 Rename-Computer -NewName "$NewName"
 #Output bitlocker id, bitlocker key, PC model, CPU, serienummer en drive info naar een tekst bestand
-$specs = New-Object psobject
-$specs | Add-Member -MemberType NoteProperty -Name Model -Value $Model
-$specs | Add-Member -MemberType NoteProperty -Name CPU -Value $CPU
-$specs | Add-Member -MemberType NoteProperty -Name SerieNummer -Value $SerieNummer
-$specs | Add-Member -MemberType NoteProperty -Name SchijfNaam -Value $SchijfNaam
-$specs | Add-Member -MemberType NoteProperty -Name SchijfGrootte -Value $SchijfGrootte
-$specs | Add-Member -MemberType NoteProperty -Name RAMInfo -Value $VolledigeRamInfo
-$specs | Add-Member -MemberType NoteProperty -Name BitLockerID -Value $BitLockerID
-$specs | Add-Member -MemberType NoteProperty -Name BitLockerHerstelSleutel -Value $BitLockerSleutel
+$Specs = [PSCustomObject]@{
+    Model                       = "$Model"
+    CPU                         = "$CPU"
+    SerieNummer                 = "$SerieNummer"
+    SchijfInfo                  = "$SchijfGrootte GiB $SchijfNaam"
+    RAMInfo                     = "$TotaalGeheugen GB @ $GeheugenMHZ MHZ"
+    "Bitlocker ID"              = "$BitlockerID"
+    "Bitlocker Herstel Sleutel" = "$BitLockerSleutel"
+}
 $specs | Export-Csv H:\PcInfo$NewName.csv -NoTypeInformation
 #Verandert tijdzone
 tzutil /s "W. Europe Standard Time"
