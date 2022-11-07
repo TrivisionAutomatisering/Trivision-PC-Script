0bd2142e6d2b3bad432622f8715f4a5c88bb202b
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-11-07 13:48:46 +0100

    Update Resources/Semi-Auto install.ps1

--- Resources/Semi-Auto install.ps1
+++ Resources/Semi-Auto install.ps1
@@ -12,11 +12,11 @@ Write-Host "Na dit script word de pc opnieuw opgestart."
 #De-blokkeert ps1 bestanden voor windows 10
 Get-ChildItem H:\Resources | Unblock-File
 #Download TeamViewer
-wget https://trivision.nl/downloads/TeamViewer_Host_Setup.exe -OutFile "C:\TeamViewer.exe"
+Invoke-WebRequest https://trivision.nl/downloads/TeamViewer_Host_Setup.exe -OutFile "C:\TeamViewer.exe"
 #Voert TeamViewer uit
 & "C:\TeamViewer.exe"
 #Download ninite
-wget https://ninite.com/7zip-adoptjdkx8-chrome-foxit/ninite.exe -OutFile "C:\ninite.exe"
+Invoke-WebRequest https://ninite.com/7zip-adoptjdkx8-chrome-foxit/ninite.exe -OutFile "C:\ninite.exe"
 #Voert ninite uit
 & "C:\ninite.exe"
 Read-Host 'Druk op Enter als TeamViewer en ninite geinstalleerd zijn.'
