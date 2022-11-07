3034f599ae6f74ef086727449e626aa75415b3a3
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-11-07 17:00:48 +0100

    Deleting Start script als admin.bat

--- Start script als admin.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set /p "ScriptDrive=Wat is de schijf letter van de usb?(bijv. D)(zonder :)"
-powershell.exe -ExecutionPolicy UnRestricted -File "%ScriptDrive%:\Resources\Semi-Auto install.ps1" "%ScriptDrive%"
