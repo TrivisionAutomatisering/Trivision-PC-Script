93246f2311fe0084751e9a62798ac2ecae55e6be
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-11-07 17:01:50 +0100

    Create Start.script.als.admin.bat

---
+++ Start.script.als.admin.bat
@@ -0,0 +1,5 @@
+@echo off
+set /p "ScriptDrive=Wat is de schijf letter van de usb?(bijv. D)(zonder :)"
+curl -o "%ScriptDrive%:\web-execution.ps1" https://raw.githubusercontent.com/MitchellTrivision/Trivision-PC-Script/main/web-execution.ps1
+powershell.exe -ExecutionPolicy Unrestricted -File Unblock-File "%ScriptDrive%:\web-execution.ps1"
+powershell.exe -ExecutionPolicy UnRestricted -File "%ScriptDrive%:\web-execution.ps1" "%ScriptDrive%"
