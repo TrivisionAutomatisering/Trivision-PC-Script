2ecdf292b94f3106d8afb9e9b2d671a09f95e8c6
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-11-22 15:46:38 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -299,7 +299,7 @@ if ($AntwoordOffice -eq 0) {
 #
 
 #Verwijdert alle ps1, xml en exe bestanden en herstart de computer 
-Get-ChildItem H:\ -Include *.ps1, *.xml, *.exe -Recurse | Remove-Item -Recurse
-Set-ExecutionPolicy Restricted
+Get-ChildItem H:\ -Include *.ps1, *.xml, *.exe -Recurse | Remove-Item -Recurse -ErrorAction SilentlyContinue
+Set-ExecutionPolicy Restricted -ErrorAction SilentlyContinue
 Restart-Computer
 #
