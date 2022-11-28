9ade7725ef4ec5c99581fc5bb49b765b78336fbf
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-11-28 08:57:35 +0100

    Deleting web-execution.ps1

--- web-execution.ps1
+++
@@ -1,9 +0,0 @@
-#Neemt variabele over vanuit het cmd start script
-param ([string]$ScriptDrive)
-#Verandert usb schijf letter naar H:
-Get-Partition -DriveLetter $ScriptDrive | Set-Partition -NewDriveLetter H
-mkdir C:\Temp -ErrorAction Ignore
-Invoke-WebRequest https://github.com/MitchellTrivision/Trivision-PC-Script/releases/latest/download/PcScript.zip -OutFile C:\Temp\PcScript.zip
-Expand-Archive C:\Temp\PcScript.zip -DestinationPath H:\ -Force
-Remove-Item "C:\Temp" -Recurse
-powershell -ExecutionPolicy UnRestricted -File "H:\Resources\Semi-Auto install.ps1"
