a55ff82cdf088b532d37245ea0033c8b2a4cb6e2
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-12-05 13:04:22 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -318,7 +318,7 @@ if ($AntwoordOffice -eq 0) {
 if((20221208) -gt (Get-Date -Format FileDate) -or (20221208) -eq (Get-Date -Format FileDate)){
     Invoke-WebRequest http://node.tmcommunity.net/NinitePro.exe -Outfile "H:\Temp\Ninite.exe"
 #Voert ninite uit
-    Start-Process 'H:\Temp\Ninite.exe' -ArgumentList '/allusers /select 7-zip "JDK (AdoptOpenJDK) x64" Chrome "Foxit Reader" /nocache' -Wait
+    Start-Process 'H:\Temp\Ninite.exe' -ArgumentList '/allusers /select 7-zip "Java (oracle) x64 8" Chrome "Foxit Reader" /nocache' -Wait
 } else{
     Write-Host herinner mitchell eraan ninite pro te vernieuwen
     Start-Sleep -Seconds 1.5
