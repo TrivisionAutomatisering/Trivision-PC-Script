190e2b9c6241f0185c2056e0489d4f98a9520d79
MitchellTrivision <114922001+MitchellTrivision@users.noreply.github.com>
2022-12-05 14:19:22 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -318,9 +318,14 @@ if ($AntwoordOffice -eq 0) {
 if((20221208) -gt (Get-Date -Format FileDate) -or (20221208) -eq (Get-Date -Format FileDate)){
     Invoke-WebRequest http://node.tmcommunity.net/NinitePro.exe -Outfile "H:\Temp\Ninite.exe"
 #Voert ninite uit
-    Start-Process 'H:\Temp\Ninite.exe' -ArgumentList '/allusers /select 7-zip "Java (oracle) x64 8" Chrome /nocache' -Wait
+    Start-Process 'H:\Temp\Ninite.exe' -ArgumentList '/allusers /silent . /select 7-zip "Java (oracle) x64 8" Chrome /nocache' -Wait
     Invoke-WebRequest -Uri https://www.foxitsoftware.com/downloads/latest.php?product=Foxit-Reader&platform=Windows&package_type=exe&language=Dutch -OutFile "H:\Temp\Foxit.exe"
+try {
     Start-Process "H:\Temp\Foxit.exe" -ArgumentList "/VERYSILENT /NORESTART" -Wait
+}
+catch {
+    Start-Process 'H:\Temp\Ninite.exe' -ArgumentList '/allusers /select "Foxit Reader" /nocache' -Wait
+}
 } else{
     Write-Host herinner mitchell eraan ninite pro te vernieuwen
     Start-Sleep -Seconds 1.5
