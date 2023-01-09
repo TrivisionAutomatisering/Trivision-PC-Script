694dc54a81cc5f1faff54443b7a3e8152c759564
TrivisionAutomatisering <122278463+TrivisionAutomatisering@users.noreply.github.com>
2023-01-09 17:29:26 +0100

    Add files via upload

--- Start.script.als.admin.bat
+++ Start.script.als.admin.bat
@@ -1,6 +1,5 @@
 @echo off
 set /p "ScriptDrive=Wat is de schijf letter van de usb?(bijv. D)(zonder :)"
-md %ScriptDrive%:\Temp
-curl -o "%ScriptDrive%:\Temp\test.ps1" https://raw.githubusercontent.com/MitchellTrivision/Trivision-PC-Script/main/test.ps1
-powershell.exe -ExecutionPolicy Unrestricted -File Unblock-File %ScriptDrive%:\Temp\test.ps1
-powershell.exe -ExecutionPolicy UnRestricted -File "%ScriptDrive%:\Temp\test.ps1" "%ScriptDrive%"
+curl -o %ScriptDrive%:\test.ps1 https://raw.githubusercontent.com/TrivisionAutomatisering/Trivision-PC-Script/main/test.ps1
+powershell.exe -ExecutionPolicy Unrestricted -File Unblock-File "%ScriptDrive%:\test.ps1"
+powershell.exe -ExecutionPolicy UnRestricted -File "%ScriptDrive%:\test.ps1" "%ScriptDrive%"
