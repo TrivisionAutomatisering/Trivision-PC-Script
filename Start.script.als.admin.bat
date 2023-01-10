092ed6993521262fbebf1c9622e88f4a47b6be3f
TrivisionAutomatisering <122278463+TrivisionAutomatisering@users.noreply.github.com>
2023-01-10 11:15:05 +0100

    Update Start.script.als.admin.bat

--- Start.script.als.admin.bat
+++ Start.script.als.admin.bat
@@ -3,5 +3,5 @@ echo Hier is een lijst met aangesloten drives
 wmic logicaldisk get deviceid, volumename, description
 set /p "ScriptDrive=Wat is de schijf letter van de usb?(bijv. D)(zonder :)"
 curl -o %ScriptDrive%:\test.ps1 https://raw.githubusercontent.com/TrivisionAutomatisering/Trivision-PC-Script/main/test.ps1
-powershell.exe -ExecutionPolicy Unrestricted -File Unblock-File "%ScriptDrive%:\test.ps1"
+powershell.exe -ExecutionPolicy UnRestricted -File Unblock-File "%ScriptDrive%:\test.ps1"
 powershell.exe -ExecutionPolicy UnRestricted -File "%ScriptDrive%:\test.ps1" "%ScriptDrive%"
